f15f8bfb3dc6611e6041570358fb8c12752396ac
unknown <pranshuadhwaryu19@gmail.com>
2022-08-19 19:51:47 +0530

    'initial'

---
+++ server.js
@@ -0,0 +1,22 @@
+const express= require('express')
+const app= express()
+const PORT=process.env.PORT || 3000
+
+const ejs=require('ejs')
+const expressLayout =require('express-ejs-layouts')
+const path=require('path')  //inbuilt-lib
+
+
+app.get('/',(req,res)=>{
+    res.render('home')  //here you can paste html file
+})
+
+//set template engine
+app.use(expressLayout)
+app.set('views',path.join(__dirname,'/resources/views'))  //this append the path home.ejs file to resources/views
+app.set('view engine','ejs')
+
+app.listen(PORT, ()=>{
+    console.log(`Listening on port ${PORT}`)
+})
+
